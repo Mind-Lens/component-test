3124503a8b4bc0d15e38d3926a5c43fbd8565e1f
mindlens-yongy <alan@mindlens.com.sg>
2024-06-07 11:24:52 +0800

    added storybook

---
+++ src/stories/Alert.stories.ts
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+import "../../dist/index.js"
+
+const meta: Meta = {
+  title: 'LitPlayground/Alert',
+  tags: ['autodocs'],
+  render: (args) => html`
+    <lit-alert class="alert ${args.color}" header="${args.header}" content=${args.content}>
+    </lit-alert>
+  `,
+  argTypes: {
+    color: {
+      control: { type: 'select' },
+      options: [
+        'alert-info',
+        'alert-success',
+        'alert-warning',
+        'alert-error',
+      ]
+    }
+  },
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Info: Story = {
+  args: {
+    color: "alert-info",
+    header: "some title here",
+    content: "content goes here"
+  }
+}
---
+++ src/stories/Card.stories.ts
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+import "../../dist/index.js"
+
+const meta: Meta = {
+  title: 'LitPlayground/Card',
+  tags: ['autodocs'],
+  render: (args) => html`
+    <lit-card class="card" .data="${args.data}"></lit-card>
+  `,
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Info: Story = {
+  args: {
+    data: {
+      header: "card header", content: "card content here",
+    }
+  }
+}
