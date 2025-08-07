c5da0e004076330a4b7a01122c277a9688c9f617
FranCeballos <104146989+FranCeballos@users.noreply.github.com>
2025-08-07 02:24:00 -0300

    Remove irrelevant Lit components

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 // entry point for all lit components in the library
 // export has to be explicitly named, no * exports
 export { LitButton } from './Button/Button';
-export { LitAlert } from './Alert/Alert';
 export { LitCard } from './Card/Card';
-export { LitList } from './Lists/Lists';
 
 // working package version ml-lit-components@0.0.8
 // html,vue, angular usable v0.0.19 = built with index.ts
--- src/react/index.ts
+++ src/react/index.ts
@@ -1,5 +1,1 @@
 // lit react wrapper index
-export { LitReactButton } from '../Button/Button.wrapper';
-export { LitReactAlert } from '../Alert/Alert.wrapper';
-export { LitReactCard } from '../Card/Card.wrapper';
-export { LitReactList } from '../Lists/Lists.wrapper';
